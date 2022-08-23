635c919c17dc93cf8a92a3c32c128b5d399bd451
Alan Chelko <achelko@gmail.com>
2022-08-23 14:32:45 +0200

    Finally got to proof of life with API

--- src/App.js
+++ src/App.js
@@ -10,57 +10,49 @@ class App extends React.Component {
       cityData: [],
       // error: false,
       // errorMessage: ''
-  
     }
   }
-  
-
-handleInput = (e) => {
-  e.preventDefault();
-  this.setState({
-    city: e.target.value
-  })
-}
-
-getCityData = async (e) => {
-  e.preventDefault();
-
-   let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json;`
-       
-  
-    let cityData = await axios.get(url)
-    console.log(cityData.data);
+ 
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    })
+  }
+ 
+  getCityData = async (e) => {
+    e.preventDefault();
+    // let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json;`
+    // let url = `https://us1.locationiq.com/v1/search?key=pk.e0f5c2dfa7500990d98d5aa3afd7b48e&q=${this.state.city}&format=json`;
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
    
+    let cityData = await axios.get(url);
+    console.log(cityData);
+    // FOR YOUR LAB YOU WILL NEED TO GET A MAP IMAGE SRC. Example:
+    // `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=47.6038321,-122.3300624&zoom=10`
+        
+  }
+
+  render(){
+
+    return (
+      <>
+      
+      <form onSubmit={this.getCityData}>
+        <label> Pick a city!
+          <input type="text" onInput={this.handleInput}/>
+        </label>
+        <button type="submit">Explore!</button>
+      </form>
+
+      {
+        <p>{this.state.city}</p>
+      }
+
+        </>
+    );
+  }
 }
-
-render(){
-
-  return (
-    <>
-     
-     <form onSubmit={this.getCityData}>
-      <label> Pick a city!
-        <input type="text" onInput={this.handleInput}/>
-      </label>
-      <button type="submit">Explore!</button>
-     </form>
-
-     {/* {
-       this.state.error
-       ?
-       <p>{this.state.errorMessage}</p>
-       :
-       console.log(') //placeholder
-       
-     } */}
-
-    </>
-  );
-}
-}
-
-
-
 
 
 
