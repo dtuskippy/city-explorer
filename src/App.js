e3f216bd171154da535d325bc318cebab5ef7b0f
Alan Chelko <achelko@gmail.com>
2022-08-26 15:21:11 +0200

    Was able to render map, weather ul, movie url all with conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 
 
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       city: '',
-      cityData: [],
-      weatherCity: '',
+      showCity: false,
+      cityLat: '',
+      cityLon: '',
       weatherData: {},
-      showWeather: false
-      // error: false,
-      // errorMessage: ''
+      moviesData: []
     }
   }
  
@@ -22,63 +22,41 @@ class App extends React.Component {
     e.preventDefault();
     this.setState({
       city: e.target.value
+      
     })
   }
 
-  handleInputX = (e) => {
-    e.preventDefault();
-    this.setState({
-      weatherCity: e.target.value
-    })
-  }
- 
   getCityData = async (e) => {
     e.preventDefault();
-    // let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json;`
-    // let url = `https://us1.locationiq.com/v1/search?key=pk.e0f5c2dfa7500990d98d5aa3afd7b48e&q=${this.state.city}&format=json`;
-    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
-   
-    let cityData = await axios.get(url);
-    
-    let lat = cityData.data[0].lat;
-    let lon = cityData.data[0].lon;
-    let coordinates = [];
-    coordinates.push(lat, lon);
+       
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+      let cityData = await axios.get(url);
 
-    let latLog = console.log(cityData.data[0].lat);
-    let lonLog = console.log(cityData.data[0].lon);
-    console.log(latLog);
-    console.log(lonLog);
-
-    this.setState({
-      cityData: coordinates
+      let moviesUrl = `${process.env.REACT_APP_SERVER}/movies?city=${this.state.city}`
+      let moviesResponse = await axios.get(moviesUrl);
       
-    });
-
-    }
-
-
-    handleSubmit = async (e) => {
-    e.preventDefault();
-
-    let url = `${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.weatherCity}`
-    let weatherData = await axios.get(url);
-
-    console.log(weatherData.data);
-    this.setState({
-      weatherData: weatherData.data,
-      showWeather: true
-    })
-    
-    }
-    // 
-    // FOR YOUR LAB YOU WILL NEED TO GET A MAP IMAGE SRC. Example:
-    // `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=47.6038321,-122.3300624&zoom=10`
-        
+      let lat = cityData.data[0].lat;
+      let lon = cityData.data[0].lon;
   
+      let urlWeather = `${process.env.REACT_APP_SERVER}/weather?lat=${lat}&lon=${lon}`
+      let weatherResponse = await axios.get(urlWeather);
+
+      this.setState({
+        showCity: true,
+        cityLat: lat,
+        cityLon: lon,
+        weatherData: weatherResponse.data,
+        moviesData: moviesResponse.data
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
 
   render(){
-
+    console.log(this.state);
     return (
       <>
       
@@ -89,31 +67,55 @@ class App extends React.Component {
         </label>
         <button type="submit">Explore!</button>
       </form>
-      
-      
-      <h1>Find City Weather</h1>
-      <form onSubmit={this.handleSubmit}>
-        <label> 
-          <input type="text" onInput={this.handleInputX}/>
-        </label>
-        <button type="submit">Display Weather City!</button>
-      </form>
 
-      {
-          this.state.showWeather &&
-          <p>Lat: {this.state.weatherData.lat} Lon: {this.state.weatherData.lon}</p>
-      }
+    
       
-      {
-        <>
-          <p>{this.state.city}</p>
-          <p>{this.state.cityData}</p>
-         
-        </>
-      }
+    {
+      this.state.showCity && 
+
+       <div>
+         <h2>{this.state.city} is a great choice!</h2>
+         <p>Latitude: {this.state.cityLat}</p>
+         <p>Longitude: {this.state.cityLon}</p>
+         <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityLat},${this.state.cityLon}&zoom=${14}`} alt={this.state.city} width="300" />
+       </div>
+    }
+
+    {
+      this.state.weatherData.length > 0 && 
+      
+      <div>
+        <h2>{this.state.city}'s Next 16 Day Weather Forecast</h2>
+        <ul>
+            {
+              this.state.weatherData.map((day, idx) =>
+                <li key={idx}>{day.date}: {day.description}</li>
+              )
+            }
+        </ul>
+      </div>
+    }
+      
+    {
+      this.state.moviesData.length > 0 && 
+      
+      <div>
+        <h2>Top Movies Related to {this.state.city}</h2>
+        <ul>
+            {
+              this.state.moviesData.map((movie, idx) =>
+                <li key={idx}>{movie.date}</li>
+              )
+            }
+        </ul>
+      </div>
+    }
+   
+      
 
 
-      </>
+
+    </>
         
     );
   }
